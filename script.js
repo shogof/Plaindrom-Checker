7b39022fe015596bb694151569b2be43284729b7
Shegofa <shogofadeveloper12@gmail.com>
2024-06-24 09:58:22 +0400

    js linter error solved

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const checkBtn = document.getElementById("check-btn");
-const showResult = document.getElementById("show-result");
+const checkBtn = document.getElementById('check-btn');
+const showResult = document.getElementById('show-result');
 
-checkBtn.addEventListener("click", function () {
+checkBtn.addEventListener('click', function () {
   const userInput = document
-    .getElementById("text-input")
+    .getElementById('text-input')
     .value.trim()
     .toLowerCase();
 
-  if (userInput === "") {
-    showResult.textContent = "Please input a value";
-    showResult.style.backgroundColor = "rgba(244, 234, 54, 0.7)";
+  if (userInput === '') {
+    showResult.textContent = 'Please input a value';
+    showResult.style.backgroundColor = 'rgba(244, 234, 54, 0.7)';
     return;
   }
 
-  const cleanInput = userInput.replace(/[^a-z0-9]/g, "");
+  const cleanInput = userInput.replace(/[^a-z0-9]/g, '');
 
-  if (cleanInput === "") {
-    showResult.textContent = "Please input a valid text";
-    showResult.style.backgroundColor = "rgba(244, 234, 54, 0.7)";
+  if (cleanInput === '') {
+    showResult.textContent = 'Please input a valid text';
+    showResult.style.backgroundColor = 'rgba(244, 234, 54, 0.7)';
     return;
   }
 
@@ -25,10 +25,10 @@ checkBtn.addEventListener("click", function () {
 
   if (isPalindrome) {
     showResult.textContent = `${userInput} is a palindrome`;
-    showResult.style.backgroundColor = "rgba(127, 231, 168, 0.7)";
+    showResult.style.backgroundColor = 'rgba(127, 231, 168, 0.7)';
   } else {
     showResult.textContent = `${userInput} is not a palindrome`;
-    showResult.style.backgroundColor = "rgba(247, 44, 44, 0.6)";
+    showResult.style.backgroundColor = 'rgba(247, 44, 44, 0.6)';
   }
 });
 
