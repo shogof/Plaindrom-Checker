0ea33277792d09452c5e5740fa59f218a3f43050
Shegofa <shogofadeveloper12@gmail.com>
2024-06-24 10:31:03 +0400

    js linter error solved

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-const checkBtn = document.getElementById("check-btn");
-const showResult = document.getElementById("show-result");
-
-checkBtn.addEventListener("click", function () {
+const showResult = document.getElementById('show-result');
+const checkBtn = document.getElementById('check-btn');
+checkBtn.addEventListener('click', () => {
   const userInput = document
-    .getElementById("text-input")
+    .getElementById('text-input')
     .value.trim()
     .toLowerCase();
 
-  if (userInput === "") {
-    showResult.textContent = "Please input a value";
-    showResult.style.backgroundColor = "rgba(244, 234, 54, 0.7)";
+  if (userInput === '') {
+    showResult.textContent = 'Please input a value';
+    showResult.style.backgroundColor = 'rgba(244, 234, 54, 0.7)';
     return;
   }
 
-  const cleanInput = userInput.replace(/[^a-z0-9]/g, "");
+  const cleanInput = userInput.replace(/[^a-z0-9]/g, '');
 
-  if (cleanInput === "") {
-    showResult.textContent = "Please input a valid text";
-    showResult.style.backgroundColor = "rgba(244, 234, 54, 0.7)";
+  if (cleanInput === '') {
+    showResult.textContent = 'Please input a valid text';
+    showResult.style.backgroundColor = 'rgba(244, 234, 54, 0.7)';
     return;
+  }
+
+  function checkPalindrome(input) {
+    const len = input.length;
+    for (let i = 0; i < len / 2; i += 1) {
+      if (input[i] !== input[len - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
   }
 
   const isPalindrome = checkPalindrome(cleanInput);
 
   if (isPalindrome) {
     showResult.textContent = `${userInput} is a palindrome`;
-    showResult.style.backgroundColor = "rgba(127, 231, 168, 0.7)";
+    showResult.style.backgroundColor = 'rgba(127, 231, 168, 0.7)';
   } else {
     showResult.textContent = `${userInput} is not a palindrome`;
-    showResult.style.backgroundColor = "rgba(247, 44, 44, 0.6)";
+    showResult.style.backgroundColor = 'rgba(247, 44, 44, 0.6)';
   }
 });
-
-function checkPalindrome(input) {
-  const len = input.length;
-  for (let i = 0; i < len / 2; i++) {
-    if (input[i] !== input[len - 1 - i]) {
-      return false;
-    }
-  }
-  return true;
-}
